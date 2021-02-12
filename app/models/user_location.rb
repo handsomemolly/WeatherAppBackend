c3be00bea01c5d4df2d00a5c1818cea50e31ace6
handsomemolly <ryan.kendig@gmail.com>
2021-02-12 11:35:08 -0800

    api hidden

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -3,7 +3,7 @@ class UserLocation < ApplicationRecord
   belongs_to :location
 
   def self.get_request
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=37.7749&lon=-122.4194&units=imperial&exclude={part}&appid=bfb0f9ce0f1c23abb82d4e2a43fba953"
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=37.7749&lon=-122.4194&units=imperial&exclude={part}&appid=#{API}"
     JSON.parse(RestClient.get(url))
   end
 end
