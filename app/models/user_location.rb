7fd090a2995a2c3c739f7ad7ce2000b9d513c477
handsomemolly <ryan.kendig@gmail.com>
2021-02-12 11:55:11 -0800

    api hidden and fixed

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -1,9 +1,12 @@
+# require '../../APIkey2.rb'
+
 class UserLocation < ApplicationRecord
   belongs_to :user
   belongs_to :location
 
   def self.get_request
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=37.7749&lon=-122.4194&units=imperial&exclude={part}&appid=#{API}"
+    api = ENV["WEATHER_API_KEY"]
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=37.7749&lon=-122.4194&units=imperial&exclude={part}&appid=#{api}"
     JSON.parse(RestClient.get(url))
   end
 end
